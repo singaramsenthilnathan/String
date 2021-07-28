709910c9715c326491fbc030bef564b0022ba52b
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-28 13:44:39 +0530

    String

---
+++ Space1.java
@@ -0,0 +1,10 @@
+package test;
+public class Space1
+{
+public static void main(String[] args) 
+{
+String str = " India is My Country ";
+String nospace=str.replaceAll("\\s","");
+System.out.println(nospace);
+}
+}
