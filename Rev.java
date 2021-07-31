2b73b35b607b3fb0c67af49c0ee86a6b336c07b8
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-31 19:37:44 +0530

    Reverse String

---
+++ Rev.java
@@ -0,0 +1,14 @@
+package test;
+public class Rev
+{
+public static void main(String[] args) 
+{
+String st="SingaramSenthil";
+char[] ch=st.toCharArray();
+
+for(int i=ch.length-1;i>=0;i--)//15-1//14>0
+{
+System.out.print(ch[i]);//l//
+}
+}
+}
